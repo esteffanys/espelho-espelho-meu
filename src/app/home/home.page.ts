9ea34627b6919886eb3107640aec6e1584737af3
esteffanys <silmara.e@escolar.ifrn.edu.br>
2023-06-19 17:26:52 -0300

    espelho

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CameraPreview } from '@capacitor-community/camera-preview';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+    CameraPreview.start({ parent: "cameraPreview"});
+  }
 
 }
